4d031b7373ad3e1f7a9d42996271bcfd66ae17a5
hansin <hansin.ciputra@gmail.com>
2015-08-12 23:46:03 +0700

    complete the CRUD

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -9,10 +9,6 @@ class InventoriesController < ApplicationController
     @inventory = Inventory.new
   end
 
-  def edit #to display the edit form
-    
-  end
-  
   def create #no view, only process the form in the new actions
     @inventory = Inventory.new(params.require(:inventory).permit(:pName, :pQuantity, :pMeter, :pWeight, :pSellPrice, :pCategory, :pPic))
     
@@ -22,11 +18,26 @@ class InventoriesController < ApplicationController
       render "new"
     end
   end
+  
+  def edit #to display the edit form
+    @inventory = Inventory.find(params[:id])
+  end
+  
   def update #no view, only process the form in edit actions
+    @inventory = Inventory.find(params[:id]) #this to find the post that we wanted to update, the id is from parameters when submit button is pressed
     
+    #get the inventory parameters from user input , we get the data from from_for @inventory in edit view
+    if @inventory.update_attributes(params.require(:inventory).permit(:pName, :pQuantity, :pMeter, :pWeight, :pSellPrice, :pCategory, :pPic))
+      redirect_to inventories_path , :notice => "Inventori Berhasil Terupdate"
+    else
+      render "edit"
+    end
   end
   def destroy #no view, only process 
-    
+    @inventory = Inventory.find(params[:id])
+    #simply use .destroy method to delete inventory 
+    @inventory.destroy
+    redirect_to inventories_path, :notice => "Inventori Telah Dihapus"
   end
   
 end
--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,2 +1,7 @@
 class Inventory < ActiveRecord::Base
+  #check ruby guides for full validates helper
+  validates :pName, :pQuantity, :pMeter, :pWeight, :pSellPrice, :pCategory, :presence => true
+  validates :pName, :uniqueness => {:messages => "Nama Barang Sudah Ada"}
+  validates :pQuantity,:pWeight,:pSellPrice, :numericality => true
+  
 end
