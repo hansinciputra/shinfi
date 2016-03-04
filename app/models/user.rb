2def442669a064016bfe6d8078d9b51040d6fd41
hansinciputra <hansin.ciputra@gmail.com>
2016-03-04 16:51:56 +0700

    name experiment3

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ def encrypt_password
 end
 
 def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid, email:auth.info.email, name:auth.info.name).first_or_initialize.tap do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
