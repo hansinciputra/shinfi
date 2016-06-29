900aebf6c513cdf1ab00a606d16c165495916f58
hansinciputra <hansin.ciputra@gmail.com>
2016-06-29 19:59:59 +0700

    file fog

--- app/uploaders/workshop_image_uploader.rb
+++ app/uploaders/workshop_image_uploader.rb
@@ -7,8 +7,8 @@ class WorkshopImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  #storage :fog
-  storage :file
+  storage :fog
+  #storage :file
 
   include CarrierWave::MimeTypes
   process :set_content_type
